214102482968c24332d5e57b745be18ee3dc0103
Emil Soman <emil.soman@gmail.com>
2016-05-05 13:28:59 +0530

    Use stricter === for string comparison

--- app/content_script.js
+++ app/content_script.js
@@ -1,9 +1,9 @@
 import { requestStore } from './store'
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if(request.message == "LOG_REQUEST"){
+  if(request.message === "LOG_REQUEST"){
     requestStore.dispatch({type: "ADD_REQUEST", request: request.request});
-  } else if(request.message == "GET_STORE") {
+  } else if(request.message === "GET_STORE") {
     sendResponse({requests: requestStore.getState()});
   }
 });
