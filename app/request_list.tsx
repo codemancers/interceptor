a1255c3069fb159a6f477ebcac2ed7d7fd8aaff9
Amit Bhavikatti <amit@codemancers.com>
2018-02-12 18:50:51 +0530

    Add fuzzy search and tests for Request-list,update react

--- app/request_list.tsx
+++ app/request_list.tsx
@@ -1,40 +1,68 @@
 /// <reference path="../node_modules/@types/chrome/chrome-app.d.ts" />
 import * as React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next'
+import filterFactory, { textFilter, selectFilter } from 'react-bootstrap-table2-filter';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 interface RowProps {
   request : chrome.webRequest.WebRequestDetails,
-  key : number,
+  keyValue : number,
   handleIntercept: React.MouseEventHandler<{}>
-}
-
-export const Row = ( props: RowProps) => {
-  return (<tr key={props.key}>
-      <td className="url">{props.request.url}</td>
-      <td className="method">{props.request.method}</td>
-      <td><button type="button" className="btn-clear" onClick={props.handleIntercept.bind(this, props.request.url, props.request.method, 404)}>Intercept</button></td>
-    </tr>);
 }
 
 export interface RequestObj { requests: Array<chrome.webRequest.WebRequestDetails>, handleIntercept : React.MouseEventHandler<{}> }
 
  const RequestList = (props: RequestObj) => {
+  function timeFormatter(cell, row) {
+    if (row.timeStamp) {
+      return new Date(row.timeStamp).toTimeString()
+    }
+
+    return ({ cell });
+  }
+  function addButton() {
+      return <button onClick={props.handleIntercept}>Intercept</button>
+    }
+  interface methodInterface {
+    GET : string;
+    POST : string;
+    OPTIONS : string;
+    PUT : string
+  }
+  const methodSelectOptions:methodInterface = {
+    'GET' : 'GET',
+    'POST' : 'POST',
+     'OPTIONS': 'OPTIONS',
+    'PUT' : 'PUT'
+  };
   return(
-    <table>
-      <thead>
-        <tr>
-          <th>URLs</th>
-          <th>Method</th>
-          <th>Intercept</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          props.requests.map((requestDetail, index) => {
-            return <Row request={requestDetail} key={index} handleIntercept={props.handleIntercept} />
-          })
-        }
-      </tbody>
-    </table>
+    <BootstrapTable
+      keyField='requestId'
+      filter={ filterFactory()}
+      pagination={ paginationFactory() }
+      cellEdit={ cellEditFactory({
+        mode: 'dbclick',
+        blurToSave: true,
+        beforeSaveCell: (oldValue, newValue, row, column) => { console.log("oldValue::", oldValue,"newValue::", newValue, "row::",row, "column::", column)}
+      }) }
+      data={ props.requests}
+      columns = {[
+        {text : 'URLs', dataField : 'url', editable: true,
+         filter : textFilter(),
+         events: {
+          onClick: (e) => {console.log(e)}
+        }},
+        {text : 'Method', dataField : 'method',filter : selectFilter({ options: methodSelectOptions}) },
+        {text : 'Request Id', dataField : 'requestId', filter:textFilter()},
+        {text : 'Time', dataField :'timeStamp', formatter: timeFormatter, sort : true, clickToSelect: true},
+        {text : 'Intercept', dataField : " ", formatter : addButton, editable: false }
+      ]}
+      striped
+      hover
+      condensed
+      handleIntercept={props.handleIntercept}
+      />
   );
 }
 export default RequestList
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "[name].js"
+    filename: "[name].js",
+    crossOriginLoading: 'anonymous'
   },
   devtool: "source-map",
   resolve: {
