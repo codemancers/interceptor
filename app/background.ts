ac5556e72b91de5c354bed6afd7dbbda1e1dbcbf
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-03-13 12:22:25 +0530

    Fix logging of multiple requests with same requestId

--- app/background.ts
+++ app/background.ts
@@ -72,12 +72,15 @@ class BackgroundWorker {
       this.data[this.currentTab].enabled &&
       this.currentTab === details.tabId
     ) {
-      tabRecords.requests.push(details);
-      this.data[this.currentTab] = tabRecords;
-      chrome.browserAction.setBadgeText({
-        text: `${tabRecords.requests.length}`,
-        tabId: details.tabId
-      });
+        if (tabRecords.requests.filter((req) => req.requestId === details.requestId).length > 0) {
+          return
+        }
+        tabRecords.requests.push(details);
+        this.data[this.currentTab] = tabRecords;
+        chrome.browserAction.setBadgeText({
+          text: `${tabRecords.requests.length}`,
+          tabId: details.tabId
+        });
     }
   };
 
