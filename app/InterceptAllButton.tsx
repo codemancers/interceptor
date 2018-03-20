ae6ee3d525a1cbd67f2b3c54a6f0d3db9e234025
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-03-20 18:14:12 +0530

    Disable Intercept button unless a check is ticked

--- app/InterceptAllButton.tsx
+++ app/InterceptAllButton.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 
 export const InterceptAllButton = props => {
+  const enabledRequests = props.requests
+  .filter(request => {
+    return props.checkedReqs[request.requestId];
+  })
   return (
     <button
     id="intercept-all-btn"
+    disabled={!enabledRequests.length}
     onClick={() => {
-      const enabledRequests = props.requests
-        .filter(request => {
-          return props.checkedReqs[request.requestId];
-        })
       props.handleCheckedRequests(enabledRequests);
     }}
   >
