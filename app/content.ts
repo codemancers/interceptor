58f014c3af4d33374625b0e014d682f27050aa73
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-03-26 17:32:32 +0530

    Fix sinon-script not being loaded before interception

--- app/content.ts
+++ app/content.ts
@@ -48,7 +48,7 @@ class Intercept {
     if (selectedReqs.requestsToIntercept.length < 1 || !selectedReqs.tabId || selectedReqs.requestsToIntercept.find( (req) => req.tabId !== selectedReqs.tabId )){
       return;
     }
-    this.injectScripts(this.runInterceptor(selectedReqs));
+    this.injectScripts(this.runInterceptor, selectedReqs);
     this.store.dispatch(sendSuccessMessage("Interception Success!"))
   };
 
@@ -94,7 +94,7 @@ class Intercept {
            });
            this.server.respondWith((xhr, id) => {
              const respondUrl = requestArray.requestsToIntercept.find((request) => {
-              if(xhr.url === request.url){
+              if(xhr.url === request.url && requestArray.responseText[request.requestId] && requestArray.contentType[request.requestId] && requestArray.statusCodes[request.requestId]){
                 xhr.respond(Number(requestArray.statusCodes[request.requestId]), { "Content-Type": requestArray.contentType[request.requestId] },requestArray.responseText[request.requestId].toString())
               }
              })
@@ -111,7 +111,7 @@ class Intercept {
     (document.head || document.documentElement).appendChild(script);
     }
 
-  injectScripts = (callback:GenericCallback) => {
+  injectScripts = (callback:GenericCallback, selectedReqs) => {
     let sinonScript = document.createElement("script");
     sinonScript.defer = false;
     sinonScript.src = chrome.extension.getURL("./lib/sinon.js");
@@ -120,7 +120,9 @@ class Intercept {
     if(!document.getElementById("interceptor-sinon")){
       (document.head || document.documentElement).appendChild(sinonScript);
     }
-    sinonScript.onload = callback;
+    sinonScript.onload = () => {
+      callback(selectedReqs);
+    };
   };
 }
 new Intercept().startMessageListener();
