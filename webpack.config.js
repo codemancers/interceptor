7405d3d79bcea4edea2af86dd2dc39c1cc4b9a7e
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-03-27 15:33:17 +0530

    Fix conflict with webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,11 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   context: path.join(__dirname, "app"),
   entry: {
-<<<<<<< HEAD
     popup: "./containers/app.tsx",
     background: "./background/background.ts",
     redux_bg: "./background/redux_bg.ts",
-    content: "./content/content.ts"
-=======
-    popup: "./popup.tsx",
-    background: "./background.ts",
+    content: "./content/content.ts",
     utils: "./utils/utils.ts"
->>>>>>> 56edc20c51e3deaf645c7fbbb31db2facd7c6939
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -41,21 +36,13 @@ module.exports = {
         NODE_ENV: `"${process.env.NODE_ENV}"`
       }
     }),
-<<<<<<< HEAD
     new CommonsChunkPlugin("common.js", ["popup"], "content.js"),
     new CopyWebpackPlugin([
       { from: "manifest.json" },
       { from: "index.html" },
-      { from: "styles/styles.css" },
-      { from: "./lib/*" }
-=======
-    new CommonsChunkPlugin("common.js", ["popup"], "utils.js"),
-    new CopyWebpackPlugin([
-      { from: 'manifest.json' },
-      { from: 'index.html' },
+      { from: "./lib/*" },
       { from: 'stylesheets/*' },
       { from: 'images/*' }
->>>>>>> 56edc20c51e3deaf645c7fbbb31db2facd7c6939
     ])
   ]
 };
