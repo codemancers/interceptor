9dba5df0d65ab9c8677dcde1146b8e766f58bb4b
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-06-04 11:18:03 +0530

    Remove redux-devtools-extension,Use local wepack binary to run wepack

--- app/store/popup_store.ts
+++ app/store/popup_store.ts
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, Middleware } from "redux";
 import { createLogger } from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 import aliases from "./aliases";
 import { alias } from "react-chrome-redux";
@@ -9,11 +8,10 @@ import { POPUP_PROPS } from "../types";
 
 let enhancer: any;
 if (process.env.NODE_ENV !== "production") {
-  const composeEnhancers = composeWithDevTools({});
   const logger: Middleware = createLogger({
     collapsed: true
   });
-  enhancer = composeEnhancers(applyMiddleware(alias(aliases), thunkMiddleware, logger));
+  enhancer = applyMiddleware(alias(aliases), thunkMiddleware, logger);
 } else {
   enhancer = applyMiddleware(alias(aliases), thunkMiddleware);
 }
