d38c7459be5e9faf43e39ac28fb88ff1d5bcc859
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-08-10 11:12:39 +0530

    Remove log statements

--- app/actions/addRequest.ts
+++ app/actions/addRequest.ts
@@ -2,10 +2,6 @@ export const RESET = "RESET";
 export const UPDATE_REQUEST_FIELDS = "UPDATE_REQUEST_FIELDS";
 
 export const updateAddRequestFields = ({ modal_url, modal_method, modal_error }) => {
-  console.log({
-    type: UPDATE_REQUEST_FIELDS,
-    payload: { modal_url, modal_method, modal_error }
-  });
   return {
     type: UPDATE_REQUEST_FIELDS,
     payload: { modal_url, modal_method, modal_error }
