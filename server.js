5634747d15ab07d9cb855da527d7445af175e209
Amit Bhavikatti <amitmbhavikatti@gmail.com>
2018-08-23 13:16:09 +0530

    Add helmet package

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const helmet = require("helmet");
+
 const app = express();
 const router = express.Router();
+
+app.use(helmet());
 app.use(express.static(__dirname + "/dist"));
 
 router.get("/", (req, res) => {
@@ -12,6 +16,11 @@ router.get("/faq", (req, res) => {
 });
 
 app.use("/", router);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.redirect("/"); //redirect to / on 404's
+});
 
 const server = app.listen(3000, () => {
   const {address, port} = server.address();
